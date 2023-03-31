037358a72c369ef20e186045ab58a8f348c11f28
luyosamson <luyosamson@gmail.com>
2023-03-31 23:27:53 +0300

    Created action for plants

--- app/controllers/plants_controller.rb
+++ app/controllers/plants_controller.rb
@@ -1,4 +1,21 @@
 class PlantsController < ApplicationController
+
+    def index
+
     plant=Plants.all
     render json:plant
+
+    end
+
+    def show
+        plant=Plant.find_by(id: params[:id])
+
+        if plant
+            render json:plant
+        else
+            render json:{error:"Plant not found"},status: :not_found
+        end
+
+    end
+
 end
