4b69afa5275b6cc9eec00f7832a7de7fb4044361
luyosamson <luyosamson@gmail.com>
2023-04-01 00:40:38 +0300

    Deleted Category attribute

--- db/migrate/20230331160021_create_plants.rb
+++ db/migrate/20230331160021_create_plants.rb
@@ -1,7 +1,6 @@
 class CreatePlants < ActiveRecord::Migration[7.0]
   def change
     create_table :plants do |t|
-      t.string :category
       t.string :type
       t.string :name
       t.float :price
--- db/migrate/20230331160037_create_flowers.rb
+++ db/migrate/20230331160037_create_flowers.rb
@@ -1,7 +1,6 @@
 class CreateFlowers < ActiveRecord::Migration[7.0]
   def change
     create_table :flowers do |t|
-      t.string :category
       t.string :type
       t.string :name
       t.float :price
