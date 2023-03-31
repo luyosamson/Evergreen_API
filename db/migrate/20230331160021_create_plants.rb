89dcb9a3e8abbc0a4d66430675b3a750565c8943
luyosamson <luyosamson@gmail.com>
2023-04-01 01:16:22 +0300

    Modified migration to suit my need

--- db/migrate/20230331160021_create_plants.rb
+++ db/migrate/20230331160021_create_plants.rb
@@ -1,7 +1,7 @@
 class CreatePlants < ActiveRecord::Migration[7.0]
   def change
     create_table :plants do |t|
-      t.string :type
+      t.string :plant_type
       t.string :name
       t.float :price
       t.string :image
--- db/migrate/20230331160037_create_flowers.rb
+++ db/migrate/20230331160037_create_flowers.rb
@@ -1,7 +1,7 @@
 class CreateFlowers < ActiveRecord::Migration[7.0]
   def change
     create_table :flowers do |t|
-      t.string :type
+      t.string :flower_type
       t.string :name
       t.float :price
       t.string :image
