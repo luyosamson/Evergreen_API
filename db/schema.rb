558853c6c31c94a7739cbe027a3437f1b2821d92
luyosamson <luyosamson@gmail.com>
2023-04-07 14:30:24 +0300

    Schema was modified due to introduction of new migrations

--- db/schema.rb
+++ db/schema.rb
@@ -10,21 +10,35 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_03_31_160037) do
+ActiveRecord::Schema[7.0].define(version: 2023_04_07_112202) do
   create_table "flowers", force: :cascade do |t|
-    t.bigint "seller_id", null: false
+    t.integer "seller_id", null: false
     t.string "flower_type"
     t.string "name"
     t.float "price"
     t.string "image"
-    t.string "description"
     t.boolean "inStock"
+    t.string "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["seller_id"], name: "index_flowers_on_seller_id"
+  end
+
+  create_table "orders", force: :cascade do |t|
+    t.integer "user_id", null: false
+    t.integer "flower_id", null: false
+    t.integer "plant_id", null: false
+    t.integer "quantity"
+    t.float "price"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["flower_id"], name: "index_orders_on_flower_id"
+    t.index ["plant_id"], name: "index_orders_on_plant_id"
+    t.index ["user_id"], name: "index_orders_on_user_id"
   end
 
   create_table "plants", force: :cascade do |t|
-    t.bigint "seller_id", null: false
+    t.integer "seller_id", null: false
     t.string "plant_type"
     t.string "name"
     t.float "price"
@@ -33,6 +47,54 @@ ActiveRecord::Schema[7.0].define(version: 2023_03_31_160037) do
     t.boolean "inStock"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["seller_id"], name: "index_plants_on_seller_id"
   end
 
+  create_table "profiles", force: :cascade do |t|
+    t.integer "user_id", null: false
+    t.string "name"
+    t.string "email"
+    t.string "username"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_profiles_on_user_id"
+  end
+
+  create_table "sellerprofiles", force: :cascade do |t|
+    t.integer "seller_id", null: false
+    t.string "name"
+    t.string "email"
+    t.string "username"
+    t.string "store_name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["seller_id"], name: "index_sellerprofiles_on_seller_id"
+  end
+
+  create_table "sellers", force: :cascade do |t|
+    t.string "name"
+    t.string "password"
+    t.string "email"
+    t.string "username"
+    t.string "store_name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "password"
+    t.string "email"
+    t.string "username"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_foreign_key "flowers", "sellers"
+  add_foreign_key "orders", "flowers"
+  add_foreign_key "orders", "plants"
+  add_foreign_key "orders", "users"
+  add_foreign_key "plants", "sellers"
+  add_foreign_key "profiles", "users"
+  add_foreign_key "sellerprofiles", "sellers"
 end
