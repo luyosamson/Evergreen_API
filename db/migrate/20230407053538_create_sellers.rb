440473237661b8182b23dd4763cdbe898928e134
luyosamson <luyosamson@gmail.com>
2023-04-10 22:37:53 +0300

    Rolled back migration and run migration afresh

--- db/migrate/20230407053538_create_sellers.rb
+++ db/migrate/20230407053538_create_sellers.rb
@@ -2,7 +2,7 @@ class CreateSellers < ActiveRecord::Migration[7.0]
   def change
     create_table :sellers do |t|
       t.string :name
-      t.string :password
+      t.string :password_digest
       t.string :email
       t.string :username
       t.string :store_name
