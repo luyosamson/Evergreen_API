94e3d1f4032c36d696b48db87e911bfd35c01043
luyosamson <luyosamson@gmail.com>
2023-04-10 22:38:19 +0300

    Modified schema

--- db/schema.rb
+++ db/schema.rb
@@ -73,7 +73,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_04_07_112202) do
 
   create_table "sellers", force: :cascade do |t|
     t.string "name"
-    t.string "password"
+    t.string "password_digest"
     t.string "email"
     t.string "username"
     t.string "store_name"
@@ -83,7 +83,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_04_07_112202) do
 
   create_table "users", force: :cascade do |t|
     t.string "name"
-    t.string "password"
+    t.string "password_digest"
     t.string "email"
     t.string "username"
     t.datetime "created_at", null: false
