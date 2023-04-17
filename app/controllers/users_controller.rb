0dbd83729c624cc56846a34012dcdd2df23fb7f5
luyosamson <luyosamson@gmail.com>
2023-04-17 23:23:18 +0300

    Modified

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,28 +5,17 @@ class UsersController < ApplicationController
         render json:user
     end
 
-        #SignUp/Register Function
-    # def create
-
-    #     user=User.create(user_params)
-    #     session[:user_id]=user.id
-    #     render json:user, status: :created
-
-    # end
-
-
+        # SignUp/Register Function
     def create
-  user = User.new(user_params)
-  if user.save
-    session[:user_id] = user.id
-    render json: user, status: :created
-  else
-    render json: { error: user.errors.full_messages }, status: :unprocessable_entity
-  end
-end
 
+        user=User.create(user_params)
+        session[:user_id]=user.id
+        render json:user, status: :created
+
+    end
 
     def show
+        
         user=User.find_by(id: params[:id])
         if user
             render json:user
@@ -54,7 +43,7 @@ end
 
 
 def user_params
-    params.require(:user).permit(:name,:username,:email,:password,:password_confirmation)
+    params.permit(:name,:username,:email,:password,:password_confirmation)
 
 end
 
