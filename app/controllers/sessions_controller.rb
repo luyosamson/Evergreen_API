88b7b6b945dd8f227a7bd35c9ec6123ec15898fb
luyosamson <luyosamson@gmail.com>
2023-04-17 14:18:17 +0300

    Modified create action

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,13 +1,14 @@
 class SessionsController < ApplicationController
+    
 
 #Login method
     def create
             user=User.find_by(username: params[:username])
-        if user&.authenticate(params[:password])
+        if user && user.authenticate(params[:password])
             session[:user_id]=user.id
             render json:user
         else
-            render json:{error:"Invalid Username or Password"}
+            render json:{error:"Invalid Username or Password"}, status: :unauthorized
         end
 
     end
@@ -18,3 +19,4 @@ class SessionsController < ApplicationController
         head :no_content
     end
 end
+
