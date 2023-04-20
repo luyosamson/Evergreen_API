aa4f881b61798749d2e3a71812fb2ea54b89fc2a
luyosamson <luyosamson@gmail.com>
2023-04-20 19:33:48 +0300

    Implimented registration of seller

--- app/controllers/sellers_controller.rb
+++ app/controllers/sellers_controller.rb
@@ -1,9 +1,13 @@
 class SellersController < ApplicationController
 
+    #Display all seller
+
     def index
         seller=Seller.all
         render json:seller
     end
+
+    #Display a particular seller
 
     def show
         seller=Seller.find_by(id: params[:id])
@@ -13,4 +17,23 @@ class SellersController < ApplicationController
             render json:{error:"Seller not found"}, status: :not_found
         end
     end
+
+    # SignUp/Register Function
+    def create
+
+        seller=Seller.create(seller_params)
+        session[:seller_id]=seller.id
+        render json:seller, status: :created
+
+    end
+  
+
+
+    private
+
+    def seller_params
+        params.permit(:name,:username,:store_name,:email,:password,:password_confirmation)
+
+    end
+
 end
