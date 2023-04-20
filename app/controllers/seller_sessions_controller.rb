a8f27ea548c64b5b9f7a4214209fcb1c13c54084
luyosamson <luyosamson@gmail.com>
2023-04-20 21:47:48 +0300

    Modified

--- app/controllers/seller_sessions_controller.rb
+++ app/controllers/seller_sessions_controller.rb
@@ -1,2 +1,21 @@
 class SellerSessionsController < ApplicationController
+
+
+#Login method
+    def create
+            seller=Seller.find_by(username: params[:username])
+        if seller && seller.authenticate(params[:password])
+            session[:seller_id]=seller.id
+            render json:seller
+        else
+            render json:{error:"Invalid Username or Password"}, status: :unauthorized
+        end
+
+    end
+
+    #Logout method
+    def destroy
+        session.delete :seller_id
+        head :no_content
+    end
 end
