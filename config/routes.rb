9da8d5ced3ee7f73150d3ccee687c9df543771e6
luyosamson <luyosamson@gmail.com>
2023-05-05 16:18:55 +0300

    Modified

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,11 @@
 Rails.application.routes.draw do
+  # get 'home/index'
+  root to: 'home#index'
  
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
 
-  # get 'plants', to: 'plants#index' 
-  # get 'flowers', to: 'flowers#index'
   resources :plants
   resources :flowers
 
